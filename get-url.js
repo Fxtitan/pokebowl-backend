857b07fe725cb7b206afc0ee5af9b4d43f745f84
Sharod Bailey <Sharod.Bailey@codeimmersives.comy>
2020-07-14 15:11:51 -0400

    not done

--- get-url.js
+++ get-url.js
@@ -1,12 +1,25 @@
 // Your code here.
 // Note that you do NOT need the data for this function!
-const getUrl = (subRay) => {
-  for (const poke of pokemon) {
-    if(poke.includes(subRay)) {
-      return poke
+
+const pokemon = [
+  ['bulbasaur', 'https://pokeapi.co/api/v2/pokemon/1/'],
+  ['ivysaur', 'https://pokeapi.co/api/v2/pokemon/2/'],
+  ['venusaur', 'https://pokeapi.co/api/v2/pokemon/3/'],
+  ['charmander', 'https://pokeapi.co/api/v2/pokemon/4/'],
+  ['charmeleon', 'https://pokeapi.co/api/v2/pokemon/5/'],
+  ['charizard', 'https://pokeapi.co/api/v2/pokemon/6/'],
+  ['squirtle', 'https://pokeapi.co/api/v2/pokemon/7/'],
+  ['wartortle', 'https://pokeapi.co/api/v2/pokemon/8/'],
+]
+
+const getUrl = (row) => {
+  for (const item of pokemon) {
+    if(item.includes(row)) {
+      return item[1]
     }
   }
 };
+console.log(getUrl())
 
 
 
